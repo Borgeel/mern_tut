31d73ff56bc54ae8c781080ada3d57eb2df055a0
Irfan <irfancesir93@gmail.com>
2023-02-21 02:36:30 +0100

    routes setup, error handler done

--- backend/controller/goalController.js
+++ backend/controller/goalController.js
@@ -1,15 +1,32 @@
+// @desc Get Goals
+// @route GET/api/goals
+// @access Private
 export const getGoals = (req, res) => {
   res.status(200).json({ message: "Get Goals" });
 };
 
+// @desc Set Goal
+// @route POST/api/goals
+// @access Private
 export const postGoal = (req, res) => {
-  res.status(200).json({ message: "Post Goal" });
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Please access a text field");
+  }
+
+  res.status(200).json({ message: req.body.text });
 };
 
+// @desc Update Goal
+// @route PUT/api/goals/:id
+// @access Private
 export const updateGoal = (req, res) => {
-  res.status(200).json({ message: "Update Goal" });
+  res.status(200).json({ message: `Update Goal ${req.params.id}  ` });
 };
 
+// @desc Delete Goal
+// @route DELETE/api/goals/:id
+// @access Private
 export const deleteGoal = (req, res) => {
-  res.status(200).json({ message: "Delete Goal" });
+  res.status(200).json({ message: `Delete Goal  ${req.params.id}  ` });
 };
